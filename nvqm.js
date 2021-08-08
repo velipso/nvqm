63b62f9231dbc3ff5679d5bea1f44e74f78ac1b1
yyny <6576327+yyny@users.noreply.github.com>
2021-08-08 19:29:33 +0200

    fix dead code
    fix #7

--- nvqm.js
+++ nvqm.js
@@ -907,10 +907,11 @@ function mat3_det(a){
 		a00 = a[0], a01 = a[1], a02 = a[2],
 		a10 = a[3], a11 = a[4], a12 = a[5],
 		a20 = a[6], a21 = a[7], a22 = a[8];
-	return
+	return (
 		a00 * ( a22 * a11 - a12 * a21) +
 		a01 * (-a22 * a10 + a12 * a20) +
-		a02 * ( a21 * a10 - a11 * a20);
+		a02 * ( a21 * a10 - a11 * a20)
+	);
 }
 
 function mat3_identity(out){
